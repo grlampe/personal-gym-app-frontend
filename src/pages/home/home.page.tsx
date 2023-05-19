ea4f5b466b4b64205e3654019ea5ee19089691fa
Gabriel Rodrigo Lampe <grlampe@hotmail.com>
2023-05-19 19:02:40 -0300

    fix: resolvido brackground nao seguir redimensionamento da pagina

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -12,15 +12,6 @@ export function HomePage() {
 
   return (
     <>
-      <div className="d-flex flex-column bd-highlight text-right" style={{padding: '0px 30px', height: '40px'}}>
-        <p className="p-1 bd-highlight text-primary"style={{margin: '0 !important', padding: '5px 2px !important', height: '12px'}}>
-          <strong>{currentUser?.id}</strong>
-        </p>
-        <strong>{currentUser?.id}</strong>
-        <p className="p-1 bd-highlight text-primary"style={{margin: '0 !important', padding: '5px 2px !important', height: '12px'}}>
-          <strong>{currentUser?.name}</strong>
-        </p>
-      </div>
     </>
   )
 }
